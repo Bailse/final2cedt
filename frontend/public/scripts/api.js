92a756ab43961ae79f2ac273a88e52aac0306bed
Paphangkon​ Praphuttiphong <paphangkon009@gmail.com>
2025-09-16 22:05:30 +0700

    api.js remove comment code

--- frontend/public/scripts/api.js
+++ frontend/public/scripts/api.js
@@ -1,30 +1,3 @@
-// import { BACKEND_URL } from "./config.js";
-
-// export async function getItems() {
-//   const r = await fetch(`${BACKEND_URL}/questions`);
-//   return r.json(); // -> [{ _id, title, questions, results }, ...]
-// }
-// export async function createItem(item) {
-//   const r = await fetch(`${BACKEND_URL}/questions`, {
-//     method: "POST", headers: { "Content-Type": "application/json" },
-//     body: JSON.stringify(item),
-//   });
-//   return r.json(); // created doc
-// }
-// export async function updateItem(item) {
-//   const r = await fetch(`${BACKEND_URL}/questions/${item._id}`, {
-//     method: "PUT", headers: { "Content-Type": "application/json" },
-//     body: JSON.stringify(item),
-//   });
-//   return r.json(); // updated doc
-// }
-// export async function deleteItem(id) {
-//   await fetch(`${BACKEND_URL}/questions/${id}`, { method: "DELETE" });
-// }
-// export async function loadItem(id) {
-//   const r = await fetch(`${BACKEND_URL}/questions/${id}`);
-//   return r.json();
-// }
 
 import { BACKEND_URL } from "./config.js";
 
--- frontend/public/scripts/config.js
+++ frontend/public/scripts/config.js
@@ -1,1 +1,2 @@
 export const BACKEND_URL = "http://localhost:3222";
+// export const BACKEND_URL = "http://52.87.254.101"
